9c6e71782fc65e3195c7f8e2b07a1a2365a06fb8
Yumibyte <ashraigosa@gmail.com>
2023-01-08 21:54:51 -0800

    modify structure of prompt format

--- debate/prompt.py
+++ debate/prompt.py
@@ -6,16 +6,14 @@ def render_debate_prompt(agent_name: str, debate: Debate, turns_left: int) -> st
     test_debates_instance = testDebates.TestDebates("debate/inputs.json")
     # retrieve correct prompt
     current_prompt = test_debates_instance.get_current_prompt()
-    #
-    # print("CURRENT NUM: " + str(current_prompt))
-    # print("CURRENT: " + str(test_debates_instance.get_prompts()))
+    # print("CURRENT: " + current_prompt)
 
     prompt = f"""
-    "You are {agent_name}. There are {turns_left} turns left in the debate. {current_prompt}
+    You are {agent_name}. There are {turns_left} turns left in the debate. {current_prompt}
+
     {render_debate(debate, agent_name)}
     You: "
     """.strip()
 
-
     return prompt
-print(render_debate_prompt("Bob", my_debate, 5, test_debates_instance.get_))
+# print(render_debate_prompt("Bob", my_debate, 5, test_debates_instance.get_))
