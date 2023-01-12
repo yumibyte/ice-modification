b2fdd8324dd09d4f648548d3d9a4b72ccede9449
Yumibyte <ashraigosa@gmail.com>
2023-01-11 22:58:17 -0800

    modify to remove bugs. Proper writing to json file of formatted SciQ database

--- debate/formatSciQ.py
+++ debate/formatSciQ.py
@@ -4,7 +4,7 @@
 #
 # We do not want the "correct/incorrect" syntax since it causes weird behavior
 #
-
+import random
 import json
 
 # Opening JSON file
@@ -14,47 +14,55 @@ file = open('debate/SciQDataset/train.json')
 # a dictionary
 database = json.load(file)
 
-output_contents = {"input_questions": [], "input_answers" = [], "input_answer_location" = [], "support": [], "prompt_count": 0, "question_count": 0, "input_prompts": ["You are trying to win the debate using reason and evidence. Don't repeat yourself. No more than 1-2 sentences per turn."]}
+output_contents = {"input_questions": [], "input_answers": [], "input_answer_location": [], "support": [], "prompt_count": 0, "question_count": 0, "input_prompts": ["You are trying to win the debate using reason and evidence. Don't repeat yourself. No more than 1-2 sentences per turn."]}
 
 count = 0
-while (count < 5):
-    for row in database:
 
-        # turn question and possible answers into one string to be inputted into debate model
-        question = row["question"]
-        distractor1 = row["distractor1"]
-        distractor2 = row["distractor2"]
-        distractor3 = row["distractor3"]
-        answer = row["correct_answer"]
 
-        support = row["support"]
 
-        # note, distractors should be shuffled each time since ChatGPT (sister model) has shown
-        # cases where it generates the right answer for questions with the answer always first
-        random_int = randint(0, 3)
-        concatenated_question = ""
-        switch (random_int):
-            case 0:
-                concatenated_question = question + answer + distractor1 + distractor2 + distractor3
-                break
-            case 1:
-                concatenated_question = question + distractor1 + answer + distractor2 + distractor3
-                break
-            case 2:
-                concatenated_question = question + distractor1 + distractor2 + answer + distractor3
-                break
-            case 3:
-                concatenated_question = question + distractor1 + distractor2 + distractor3 + answer
-                break
-        output_contents["input_questions"].append(concatenated_question)
-        output_contents["input_answers"].append(answer)
-        output_contents["input_answer_location"].append(random_int)
-        output_contents["support"].append(support)
-        count++
+for row in database:
+
+    if (count == 5):
+        break
+
+
+    # turn question and possible answers into one string to be inputted into debate model
+    question = row["question"]
+    distractor1 = row["distractor1"]
+    distractor2 = row["distractor2"]
+    distractor3 = row["distractor3"]
+    answer = row["correct_answer"]
+
+    support = row["support"]
+
+    # note, distractors should be shuffled each time since ChatGPT (sister model) has shown
+    # cases where it generates the right answer for questions with the answer always first
+    # random.seed(10)
+
+    random_int = random.randint(1, 4)
+    concatenated_question = ""
+
+    if (random_int == 1):
+        concatenated_question = question + " 1: " + answer + " 2: " + distractor1 + " 3: " + distractor2 + " 4: " + distractor3
+
+    elif (random_int == 2):
+        concatenated_question = question + " 1: " + distractor1 + " 2: " + answer + " 3: " + distractor2 + " 4: " + distractor3
+
+    elif (random_int == 3):
+        concatenated_question = question + " 1: " + distractor2 + " 2: " + distractor1 + " 3: " + answer + " 4: " + distractor3
+
+    elif (random_int == 4):
+        concatenated_question = question + " 1: " + distractor3 + " 2: " + distractor1 + " 3: " + distractor2 + " 4: " + answer
+
+    output_contents["input_questions"].append(concatenated_question)
+    output_contents["input_answers"].append(answer)
+    output_contents["input_answer_location"].append(random_int)
+    output_contents["support"].append(support)
+    count += 1
 
 # Serializing json
 json_object = json.dumps(output_contents, indent=4)
 
 # Writing to sample.json
-with open("SciQ_Formatted.json", "w") as outfile:
+with open("debate/SciQ_Formatted.json", "w") as outfile:
     outfile.write(json_object)
